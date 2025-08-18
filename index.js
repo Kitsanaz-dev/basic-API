ba6717ea1b044f0a909143cc6fd5e62b22354085
Kitsanaz-Dev <kitsanaz.ctbd@gmail.com>
2025-08-18 14:37:36 +0700

    remove unnecessary import

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import router  from "./routes/routers.js";
 import dotenv from 'dotenv';
 import mongoDB from "./mongodb.js";
-import e from "express";
 
 dotenv.config(); 
 
